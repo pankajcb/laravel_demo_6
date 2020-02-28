c9b678c8de22eb81c29adafc2e85bc9b9100bcff
Shiva Gupta <codingbrains8@gmail.com>
2020-02-28 17:27:06 +0530

    Admin Model Chnages

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Admin;
 class AdminController extends Controller
 {
-    //
+    public function dashboard(Request $request){
+         
+    }
 }
