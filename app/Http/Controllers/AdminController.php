d2998d43b702ff67abc81dd07e2cac401aaa3a1a
dheer <codingbrains9@gmail.com>
2020-03-04 16:26:40 +0530

    commit by dheer

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,4 +9,8 @@ class AdminController extends Controller
     public function dashboard(Request $request){
          
     }
+
+    public function user_list(){
+        return ['a','b','c'];
+    }
 }
